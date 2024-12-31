035b0a2c0361c01b8d56c5e0f6898aec56759623
makabaka1880 <makabaka1880@outlook.com>
2024-12-31 22:53:04 +0800

    Initial Commit

---
+++ SSDSpeedTest/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SSDSpeedTest
+//
+//  Created by SeanLi on 12/31/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
